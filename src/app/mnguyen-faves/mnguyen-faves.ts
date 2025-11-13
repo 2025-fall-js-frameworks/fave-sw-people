f31f415d3d435037ce96198f1c52cb8ca866a970
Matthew Nguyen <mnguyen4@madisoncollege.edu>
2025-11-13 13:25:20 -0600

    Slug
    ng generate component mnguyen-faves --skip-tests

---
+++ src/app/mnguyen-faves/mnguyen-faves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mnguyen-faves',
+  imports: [],
+  templateUrl: './mnguyen-faves.html',
+  styleUrl: './mnguyen-faves.css',
+})
+export class MnguyenFaves {
+
+}
