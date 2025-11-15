a3989716d15d38f362cd0b97b8c94ceb170335bc
tsteelematc <tsteele@madisoncollege.edu>
2025-11-15 10:30:16 -0600

    Rebase, accept `master` then manually add back...
    Force push before manual changes, I learned ! ! !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,23 @@ import { Routes } from '@angular/router';
 import {CsmithFaves} from './csmith-faves/csmith-faves';
 import { AspriggsFavs } from './aspriggs-favs/aspriggs-favs';
 import { AkoroliovaFaveSwPeople } from './akoroliova-fave-sw-people/akoroliova-fave-sw-people';
+import { TsteeleFaves } from './tsteele-faves/tsteele-faves';
 
 export const routes: Routes = [
   {
     path: 'csmith',
     component: CsmithFaves
-  },
-	{
+  }
+	, {
     path: "aspriggs",
     component: AspriggsFavs
-  },
-    {
-        path: "akoroliova", 
-        component: AkoroliovaFaveSwPeople
-    }
+  }
+  , {
+      path: "akoroliova", 
+      component: AkoroliovaFaveSwPeople
+  }
+  , {
+      path: "tsteele", 
+      component: TsteeleFaves
+  }
 ];
