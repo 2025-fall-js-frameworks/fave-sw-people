8a9b410156fb468fa8b76561e3456923459a99b3
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-18 13:58:08 -0600

    expands the response page and reduces the people before sorting them

--- src/app/sw-people-service.ts
+++ src/app/sw-people-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { EMPTY, map, expand, tap, reduce } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +10,14 @@ export class SwPeopleService {
 
   public getPeopleFromSwapiApi() {
     return this.http.get<any>('https://swapi.dev/api/people').pipe(
+      expand(page => page.next ? this.http.get<any>(page.next) : EMPTY),
       map(response => response.results),
-      map(people => people.sort(
-        (a: any, b: any) => a.name.localeCompare(b.name)
+      reduce((acc: any[], people )=> [
+        ...acc,
+        ...people
+      ]),
+      map(people => people.sort((a: any, b: any) =>
+        a.name.localeCompare(b.name)
       ))
     );
   }
