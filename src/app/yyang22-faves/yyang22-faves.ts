437360ec1e0fd0b3b6dde7f7f017173c4f06fc64
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-18 13:37:06 -0600

    Real Swapi People Showing...

--- src/app/yyang22-faves/yyang22-faves.ts
+++ src/app/yyang22-faves/yyang22-faves.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-yyang22-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './yyang22-faves.html',
   styleUrl: './yyang22-faves.css',
 })
 export class Yyang22Faves {
+  private readonly peopleSvc = inject(SwPeopleService);
 
+  protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
 }
