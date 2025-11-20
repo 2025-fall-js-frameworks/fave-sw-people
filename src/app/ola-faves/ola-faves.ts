587b890de9cf5e37d48a73c269f0f8fb1c976bdb
Ola-Juwon <olajuwonsomuyiwa@gmail.com>
2025-11-20 07:48:18 -0600

    Generated component and skip test files
    ng generate component ola-faves --skip-tests

---
+++ src/app/ola-faves/ola-faves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-faves',
+  imports: [],
+  templateUrl: './ola-faves.html',
+  styleUrl: './ola-faves.css',
+})
+export class OlaFaves {
+
+}
