8bf702aa89903b31ca11961293fa8e42708f451d
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 19:03:21 -0600

    On a modal too... With the help of AI...

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
 import { AsyncPipe } from '@angular/common';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tsteele-faves',
@@ -10,6 +11,7 @@ import { AsyncPipe } from '@angular/common';
 })
 export class TsteeleFaves {
   private readonly peopleSvc = inject(SwPeopleService);
+  private readonly modalService = inject(NgbModal);
 
   protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
 
@@ -59,11 +61,39 @@ export class TsteeleFaves {
 
       // const data = await Promise.all([page1, page2]);
       const data = await Promise.any([page1, page2]);
-      console.log(data[0]);
+      console.log(data[0].name);
+      this.openModal(data[0].name);
     }
     
     catch(err) {
       console.warn(err)
     }
   }
+
+  private openModal(content: any) {
+    const modalRef = this.modalService.open(ModalContent, { size: 'sm' });
+    modalRef.componentInstance.data = content;
+  }
+}
+
+@Component({
+  selector: 'modal-content',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Message</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss()"></button>
+    </div>
+    <div class="modal-body">
+      <pre>{{ data }}</pre>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" (click)="activeModal.close()">Close</button>
+    </div>
+  `
+})
+export class ModalContent {
+  activeModal = inject(NgbActiveModal);
+  data: any;
 }
