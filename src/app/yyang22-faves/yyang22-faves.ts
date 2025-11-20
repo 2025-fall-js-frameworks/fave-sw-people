88808beb452f5f218205baf3e7a0828e3bee9f2f
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-20 13:25:40 -0600

    Promises as thenables demo...

--- src/app/yyang22-faves/yyang22-faves.ts
+++ src/app/yyang22-faves/yyang22-faves.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-yyang22-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './yyang22-faves.html',
   styleUrl: './yyang22-faves.css',
 })
 export class Yyang22Faves {
+  private readonly peopleSvc = inject(SwPeopleService);
 
+  protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
+
+  protected promiseAsThenables() {
+
+    const page1 = this.peopleSvc.getPeoplePageOne() 
+      .then(
+        data => {
+          console.log(data);
+
+          const page2 = this.peopleSvc.getPeoplePageTwo()
+          .then(
+            data => console.log(data)
+          )
+          .catch(
+            err => console.warn(err)
+          )
+        }
+      )
+      .catch(
+        err => console.warn(err)
+      )
+    ;
+
+  }
 }
