9cd80ad2708cd90bec758e8577ea2e3dcda45d42
tsteelematc <tsteele@madisoncollege.edu>
2025-11-27 10:15:29 -0600

    Make work with `any[]` and JS promises...
    Next, make work with our types/shapes ! ! !

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
 import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 
 type FaveDisplay = {
   name: string;
@@ -19,10 +19,10 @@ export class TsteeleFaves implements OnInit {
 
   private readonly peopleSvc = inject(SwPeopleService);
 
-  protected people$: Observable<any[]> | undefined;
+  protected people: any[] | undefined;
 
   async ngOnInit() {
-    this.people$ = this.peopleSvc.getPeopleFromSwapiApi();
+    this.people = await firstValueFrom(this.peopleSvc.getPeopleFromSwapiApi());
   }
 
   protected promisesAsThenables() {
