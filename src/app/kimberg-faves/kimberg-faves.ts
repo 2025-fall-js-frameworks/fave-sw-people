bd8f2448b4ad5fa5cca14f6f913bc5d071f47905
Kimberg23 <imbergk23@gmail.com>
2025-12-02 13:35:32 -0600

    Refactored people to be a signal...

--- src/app/kimberg-faves/kimberg-faves.ts
+++ src/app/kimberg-faves/kimberg-faves.ts
@@ -1,24 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
-import { AsyncPipe } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
 
 
 type FaveDisplay = {
   name: string;
   checked: boolean;
-  heightInCentimeters: string;
+  heightInCentimeters: number;
 };
 
 @Component({
   selector: 'app-kimberg-faves',
-  imports: [ AsyncPipe],
+  imports: [],
   templateUrl: './kimberg-faves.html',
   styleUrl: './kimberg-faves.css',
 })
-export class KimbergFaves {
-  private readonly peopleSvc = inject(SwPeopleService);
+export class KimbergFaves implements OnInit {
+  private peopleSvc = inject(SwPeopleService);
 
-  protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
+  protected people: WritableSignal<FaveDisplay[]> = signal([]);
+
+  async ngOnInit() {
+    const people = await firstValueFrom(
+      this.peopleSvc.getPeopleFromSwapiApi()
+    );
+
+    this.people.set(
+      people.map(
+        x => ({
+          name: x.name,
+          checked: false,
+          heightInCentimeters: Number(x.height),
+        })
+      )
+    );
+  }
 
   protected promisesAsThenables() {
     const page1 = this.peopleSvc.getPeoplePageOne()
