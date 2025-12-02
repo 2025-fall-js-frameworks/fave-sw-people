d8be65b98261442e52b3146999b42e551650598f
tsteelematc <tsteele@madisoncollege.edu>
2025-12-02 13:35:30 -0600

    Refactored people to be a signal...

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,8 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
-import { AsyncPipe } from '@angular/common';
 import { firstValueFrom } from 'rxjs';
-
 
 type FaveDisplay = {
   name: string;
@@ -12,18 +10,28 @@ type FaveDisplay = {
 
 @Component({
   selector: 'app-tsteele-faves',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './tsteele-faves.html',
   styleUrl: './tsteele-faves.css',
 })
 export class TsteeleFaves implements OnInit {
   private readonly peopleSvc = inject(SwPeopleService);
 
-  protected people: any[] | undefined;
+  protected people: WritableSignal<FaveDisplay[]> = signal([]);
 
   async ngOnInit() {
-    this.people = await firstValueFrom(
+    const people = await firstValueFrom(
       this.peopleSvc.getPeopleFromSwapiApi()
+    );
+
+    this.people.set(
+      people.map(
+        x => ({
+          name: x.name,
+          checked: false,
+          heightInCentimeters: Number(x.height),
+        })
+      )
     );
   }
 
